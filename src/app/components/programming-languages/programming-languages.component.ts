467bcdb641fb106c5d8d2728c4d255cd3efae69a
Eric Díaz <heimricusdc@gmail.com>
2022-09-13 13:53:42 +0200

    Made the responsive of the five body's component

--- src/app/components/programming-languages/programming-languages.component.ts
+++ src/app/components/programming-languages/programming-languages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-prog-langs',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./programming-languages.component.scss']
 })
 export class ProgrammingLanguagesComponent implements OnInit {
+  public colSize=4;
+  public isMobile:boolean = false;
 
-  constructor() { }
+  constructor(private breakpointObs: BreakpointObserver ) {}
 
   ngOnInit(): void {
+    this.breakpointObs.observe(['(max-width: 800px)', '(max-width: 360px)']).subscribe(() => this.threeBreakpoints());
+  }
+
+  threeBreakpoints(){
+    if(this.breakpointObs.isMatched('(max-width: 800px)')){
+      this.colSize = 2;
+    }
+    else if(this.breakpointObs.isMatched('(max-width: 360px)')){
+      this.colSize = 1;
+    }
+    else {
+      this.colSize = 4;
+    }
   }
 
 }
